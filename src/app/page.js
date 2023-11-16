b35e54fdc77811c61eea56af11fab9fac95428e5
amirreza-eb <nighttwalker95@gmail.com>
2023-11-16 17:51:58 +0330

    added APIKey const

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,18 @@ import {
   IoMdThunderstorm,
   IoMdSearch,
 } from "react-icons/io";
+import {
+  BsCloudHaze2Fill,
+  BsCloudDrizzleFill,
+  BsEye,
+  BsWater,
+  BsThermometer,
+  BsWind,
+} from "react-icons/bs";
+import { TbTemperatureCelsius } from "react-icons/tb";
+import { ImSpinner8 } from "react-icons/im";
+
+const APIKey = "4cdafd10eafff3dd0d3fab8ab517d5d7";
 
 export default function Home() {
   return <div>React App</div>;
