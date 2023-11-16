f10ece5cee227eabb39a04e3c63558559cd3873b
amirreza-eb <nighttwalker95@gmail.com>
2023-11-16 21:02:46 +0330

    added fetching and icons

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
 
 import Image from "next/image";
@@ -25,7 +27,50 @@ const APIKey = "4cdafd10eafff3dd0d3fab8ab517d5d7";
 
 export default function Home() {
   const [data, setData] = useState(null);
-  const [location, setLocation] = useState("Bucharest");
+  const [location, setLocation] = useState("London");
 
-  return <div>React App</div>;
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIKey}`;
+
+    axios.get(url).then((res) => {
+      setData(res.data);
+    });
+  }, [location]);
+  console.log(data);
+
+  if (!data) {
+    return (
+      <div>
+        <ImSpinner8 className=" text-5xl animate-spin" />
+      </div>
+    );
+  }
+
+  let icon;
+
+  switch (data.weather[0].main) {
+    case "Clouds":
+      icon = <IoMdCloudy />;
+      break;
+    case "Haze":
+      icon = <BsCloudHaze2Fill />;
+      break;
+    case "Rain":
+      icon = <IoMdRainy />;
+      break;
+    case "Clear":
+      icon = <IoMdSunny />;
+      break;
+    case "Drizzle":
+      icon = <BsCloudDrizzleFill />;
+      break;
+    case "Snow":
+      icon = <IoMdSnow />;
+      break;
+    case "Thunderstorm":
+      icon = <IoMdThunderstorm />;
+      break;
+  }
+
+  return <div className=" text-6xl">{icon}</div>;
 }
