996e945b39783cb58c8baab71b71aeac3e133727
amirreza-eb <nighttwalker95@gmail.com>
2023-11-16 17:36:47 +0330

    initialized Project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      primary: "Poppins",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        gradientBg: "url('/src/assets/img/bg.png')",
+      },
+      keyframes: {
+        shake: {
+          "0%": {
+            transform: "translate(3px, 0)",
+          },
+          "50%": {
+            transform: "translate(-3px, 0)",
+          },
+          "100%": {
+            transform: "translate(0, 0)",
+          },
+        },
+      },
+      animation: {
+        shake: "shake 150ms 2 linear",
       },
     },
   },
   plugins: [],
-}
+};
